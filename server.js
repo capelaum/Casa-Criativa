095c0f0433d38ad65be6bc2c4d97b2f41d3ed0b6
Capelaum <thecapellett@gmail.com>
2020-03-29 01:14:37 -0300

    usando nunjucks

--- server.js
+++ server.js
@@ -1,7 +1,58 @@
-const express = require('express')
-
+const express = require('express')      // express para criar e configurar o meu servidor
 const server = express();
+// console.log(server)
 
-console.log(server)
+const ideias = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+        url: "https://dragonpharmabrasil.com/"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mindset",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729069.svg",
+        title: "Yoga",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit",
+        url: "https://rpcketseat.com.br"
+    },
+]
 
-server.listen(5500)
+// configurar arquivos estáticos
+server.use(express.static("public"))
+
+// config do nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server, 
+    noCache: true,  
+})
+
+// cria rota e captura pedido do cliente para responder
+server.get("/", function(req, res) {
+
+    
+    return res.render('index.html', { ideias })
+})
+
+server.get("/ideias", function(req, res) {
+    return res.render('ideias.html')
+})
+
+// servidor ligado na porta 3000
+server.listen(3000)
